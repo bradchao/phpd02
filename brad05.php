5ba30ef951235a0eeb426156e4a37500b6323ef9
BradChao <brad@brad.tw>
2022-06-09 11:05:02 +0800

    06091104

---
+++ brad05.php
@@ -0,0 +1,12 @@
+<?php
+    $var1 = 10; $var2 = 3;
+    $var3 = $var1 + $var2;
+    $var4 = $var1 - $var2;
+    $var5 = $var1 * $var2;
+    $var6 = (int)($var1 / $var2);
+    $var7 = $var1 % $var2;
+    echo "{$var1} + {$var2} = {$var3}<br />";
+    echo "{$var1} - {$var2} = {$var4}<br />";
+    echo "{$var1} x {$var2} = {$var5}<br />";
+    echo "{$var1} / {$var2} = {$var6} ...... {$var7}<br />";
+?> 
