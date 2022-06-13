0ab8a61667b2f75984c9e5f00567086182726717
BradChao <brad@brad.tw>
2022-06-13 14:57:24 +0800

    06131457

--- pokerv4.php
+++ pokerv4.php
@@ -17,13 +17,22 @@
     // }
 
 ?>
+<!-- &spades; &hearts; &diams; &clubs; -->
+
 
 <table border="1" width="100%">
 <?php
+    $suits = ["&spades;", "<font color='red'>&hearts;</font>", 
+        "<font color='red'>&diams;</font>", "&clubs;"];
+    $values = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+
     foreach($players as $player){
+        sort($player);
         echo '<tr>';
         foreach($player as $card){
-            echo "<td>{$card}</td>";
+            $suit = $suits[(int)($card / 13)];
+            $value = $values[$card % 13];
+            echo "<td>{$suit}{$value}</td>";
         }
         echo '</tr>';
     }
