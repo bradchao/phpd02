9a111dcc5a0a5ecb0ed4fafb8062ee9a07b084f4
BradChao <brad@brad.tw>
2022-06-16 11:26:18 +0800

    06161126

---
+++ login.php
@@ -0,0 +1,8 @@
+<form action="checkAccount.php">
+    <input type="text" name="account" /><br />
+    <input type="password" name="passwd" /><br />
+    <input type="submit" value="login">
+    <button type="button" onclick="location.href='register.php'">
+        Register
+    </button>
+</form>
