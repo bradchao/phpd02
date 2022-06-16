9df3cba1a8c2c2254695ec8a221cfc3729dee790
BradChao <brad@brad.tw>
2022-06-16 10:07:44 +0800

    06161007

--- brad55.php
+++ brad55.php
@@ -9,10 +9,31 @@
         $result = $mysqli->query($sql);
         $member = $result->fetch_object();
     }
+    if (isset($_GET['updateid'])){
+        $updateid = $_GET['updateid'];
+        $account = $_GET['account'];
+        $passwd = $_GET['passwd'];
+        $realname = $_GET['realname'];
+        if (strlen(trim($passwd)) == 0){
+            $sql = "UPDATE member SET account = '{$account}'," .
+                "realname = '{$realname}' WHERE id = {$updateid}";
+        }else{
+            $passwd = password_hash($passwd, PASSWORD_DEFAULT);
+            $sql = "UPDATE member SET account = '{$account}'," .
+                "passwd = '{$passwd}', " .
+                "realname = '{$realname}' WHERE id = {$updateid}";
+        }
+        if  ($mysqli->query($sql)){
+            header("Location: brad53.php");
+        }else{
+            
+        }
+    }
 
 ?>
 Edit Profile<hr />
-<form method="POST">
+<form method="GET">
+    <input type="hidden" name="updateid" value="<?php echo $member->id; ?>" />
     <input type="text" name="account" value="<?php echo $member->account; ?>" /><br />
     <input type="password" name="passwd" /><br />
     <input type="text" name="realname" value="<?php echo $member->realname; ?>" /><br />
