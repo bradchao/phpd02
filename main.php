f369e4ea677cd1ccf4efa53dfeaeae722a8d65f4
BradChao <brad@brad.tw>
2022-06-17 12:01:54 +0800

    06171201

--- main.php
+++ main.php
@@ -11,8 +11,33 @@
     $cart = $_SESSION['cart'];
 
     $icon = base64_encode($member->icon) ;
+
+    $sql = "SELECT * FROM food ORDER BY id";
+    $result = $mysqli->query($sql);
+
 ?>
 Welcom, <?php echo $member->realname; ?><br />
 <img src="data:<?php echo $member->icontype; ?>;base64, <?php echo $icon; ?>"/>
 <hr />
 <a href="logout.php">Logout</a>
+<hr />
+<table border="1" width="100%">
+    <tr>
+        <th>Name</th>
+        <th>Tel</th>
+        <th>City</th>
+        <th>Town</th>
+        <th>Image</th>
+    </tr>
+    <?php
+        while ($food = $result->fetch_object()){
+            echo "<tr>";
+            echo "<td>{$food->name}</td>";
+            echo "<td>{$food->tel}</td>";
+            echo "<td>{$food->city}</td>";
+            echo "<td>{$food->town}</td>";
+            echo "<td><img src='{$food->pic}' height='100px'></td>";
+            echo "</tr>";
+        }
+    ?>
+</table>
