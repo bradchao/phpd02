67aae91fa3c1f48f32937c5be6fdf3dd7ab183a8
BradChao <brad@brad.tw>
2022-06-20 13:38:42 +0800

    06201338

--- Cart.php
+++ Cart.php
@@ -1,8 +1,45 @@
 <?php
     class Cart {
-        private $list;
-        function __construct(){
+        private $list;  // [] => [$pid] = qty
+        private $memberId;
+        function __construct($memberId){
             $this->list = array();
+            $this->memberId = $memberId;
         }
+
+        function addProduct($pid, $qty){
+            if (!array_key_exists($pid, $this->list)){
+                $this->list[$pid] = $qty;
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        function removeProduct($pid){
+            if (array_key_exists($pid, $this->list)){
+                unset($this->list[$pid]);
+            }
+        }
+
+        function editProduct($pid, $qty){
+            if (array_key_exists($pid, $this->list)){
+                $this->list[$pid] = $qty;
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
+        function getList(){
+            return $this->list;
+        }
+
+        function getMemberId(){
+            return $this->memberId;
+        }
+
+
     }
 ?>
